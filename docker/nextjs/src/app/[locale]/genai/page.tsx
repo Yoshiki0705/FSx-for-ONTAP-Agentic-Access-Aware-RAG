a9a968d277266e0e20f84ef13a7845e33220cf94
Yoshiki Fujiwara <40357845+Yoshiki0705@users.noreply.github.com>
2026-03-27 01:28:41 +0900

    fix: correct JSX nesting in sidebar conditional rendering

--- docker/nextjs/src/app/[locale]/genai/page.tsx
+++ docker/nextjs/src/app/[locale]/genai/page.tsx
@@ -1509,10 +1509,9 @@ function ChatbotPageContent() {
               </div>
             </div>
           </div>
-          </div>
         </div>
         )}
-        </div>
+      </div>
 
       {/* メインコンテンツ */}
       <div className="flex-1 flex flex-col min-w-0 transition-all duration-300">
